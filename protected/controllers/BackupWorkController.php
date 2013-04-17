e8d7d0f474aa41d89152dafc23a6709bf6b6a8f8
mint05 <mint05@mint05>
2013-04-17 12:21:18 +0200

    btn to exec

--- protected/controllers/BackupWorkController.php
+++ protected/controllers/BackupWorkController.php
@@ -166,4 +166,23 @@ class BackupWorkController extends Controller
 			Yii::app()->end();
 		}
 	}
+        
+        public function actionExecute(){
+            if(isset($_POST["bk_id"])){
+                $model = BackupWork::model()->findByPk($_POST["bk_id"]);
+                //get db list
+                $data = Utils::listAllDatabases();
+                //Filter only selected dbs
+                $filterDB = Utils::filterDbList($data, $model->afected_dbs);
+                //dump dbs
+                $result = Utils::dumpDatabases($filterDB,$model->desc, $model->sendToDropbox);
+
+                $log = new BackupWorkLog();
+                $log->backup_work_id = $bkId;
+                $log->date_exec = date('Ymd');
+                $log->status_messages = $result;
+                //handle errors and log
+                $log->save();
+            }
+        }
 }
